44dfa8144a7c6240eb2ddc8c4e06c13d073d6272
Nick Landas <nick.landas@gmail.com>
2022-12-14 22:20:48 -0600

    commit tree 226ea827c9c9d08aaf3debf4028420506e16b781

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(array)
+    if array.length == 0
+        puts 'The line is currently empty.'
+    else
+        array_of_people = array.map.with_index(1) do |name, index| 
+            "#{index}. #{name}"
+        end
+        string = array_of_people.join(' ')
+        puts "The line is currently: #{string}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    place_in_line = array.length
+    puts "Welcome, #{name}. You are number #{place_in_line} in line."
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+line([])
+line(['Shemp', 'Larry', 'Moe'])
+take_a_number(katz_deli, 'Roxanne')
+take_a_number(katz_deli, 'Jane')
+line(katz_deli)
+take_a_number(katz_deli, 'Layla')
+now_serving(katz_deli)
+now_serving(katz_deli)
+now_serving(katz_deli)
+now_serving(katz_deli)
+line(katz_deli)
